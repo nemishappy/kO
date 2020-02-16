caebcde48f19a74a14eeb4577431d9560a629cc0
nemishappy <Neomk24@gmail.com>
2020-02-16 15:09:33 +0700

    prepare for

--- src/pages/Register.svelte
+++ src/pages/Register.svelte
@@ -10,7 +10,7 @@
       err = "plz check username and password";
       return;
     }
-    let res = await fetch("http://localhost:1234/api/user", {
+    let res = await fetch("https://nemishappy.herokuapp.com/api/user", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: {
